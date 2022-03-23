6c593a90187d8bca3af0d3d9846eb8b02ce6730e
iris-w03 <93733201+iris-w03@users.noreply.github.com>
2022-03-23 14:15:39 -0500

    Instagram Part 1
    allows users to take and post pics

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 小元宵 on 3/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
